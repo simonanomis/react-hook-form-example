e690deb1e5ddf82e31491b299d9f16fd6ef73821
Simona Ristovska <simona.ristovska@blackboard.com>
2022-09-16 22:46:00 +0200

    basic react hook example form with use of useForm hook

--- src/BasicUsage.tsx
+++ src/BasicUsage.tsx
@@ -1,6 +1,30 @@
 import React from "react";
-import {useForm} from "react-hook-form";
+import {useForm, SubmitHandler} from "react-hook-form";
+
+type Inputs = {
+    example: string,
+    exampleRequired: string,
+}
 
 export default function BasicUsage() {
+    //Custom hook to manage the entire form.
+    const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>();
+    const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
 
+    console.log(watch("example")) // watch input value by passing the name of it
+
+    return (
+        /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
+        <form onSubmit={handleSubmit(onSubmit)}>
+            {/* register your input into the hook by invoking the "register" function */}
+            <input defaultValue="test" {...register("exampleRequired")} />
+
+            {/* include validation with required or other standard HTML validation rules */}
+            <input {...register("example", { required: true })} />
+            {/* errors will return when field validation fails  */}
+            {errors.exampleRequired && <span>This field is required</span>}
+
+            <input type="submit" />
+        </form>
+    );
 }
